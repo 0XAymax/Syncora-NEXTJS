3436293148687d7bb3adfbfd8edec70bd09c354b
FAHDCHH <133113067+FAHDCHH@users.noreply.github.com>
2025-04-03 00:46:52 +0100

    Update workspaceRoutes.js

--- BACKEND/routes/workspaceRoutes.js
+++ BACKEND/routes/workspaceRoutes.js
@@ -2,5 +2,7 @@ import express from "express"
 import { getWorkspacesByuserId } from "../controllers/workspacehandler.js"
 export const routerr = express.Router();
 
-routerr.get('/workspaces', getWorkspacesByuserId); 
+
+routerr.post('/workspaces',handleInputError,getWorkspacesByuserId); //ALL WORKSPACES
+routerr.get('/members', handleInputError,getMembersByWorkspaceId); //ALL members by workspaceId
 export default routerr;
