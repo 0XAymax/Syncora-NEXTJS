fb7ba947ae9b4928df4d5ca8b76048b1d8af82f2
FAHDCHH <133113067+FAHDCHH@users.noreply.github.com>
2025-04-03 00:45:57 +0100

    Update taskroutes.js

--- BACKEND/routes/taskroutes.js
+++ BACKEND/routes/taskroutes.js
@@ -1,14 +1,19 @@
 import { handleInputError } from "../middleware/middleware.js";
-import { getAllTasks } from "../controllers/taskhandlers.js"
-import * as taskController from "../controllers/taskhandlers.js"
+import { getAllTasks } from "../controllers/taskhandlers.js";
+import * as taskController from "../controllers/taskhandlers.js";
 import * as workspaceController from "../controllers/workspacehandler.js";
-import express from "express"
+import express from "express";
 const router = express.Router();
 
 // Middleware to verify token for all routes
-router.post('/tasks', getAllTasks); //ALL TASKS
+router.post("/tasks", getAllTasks); //ALL TASKS
 
-router.get('/usertasks', taskController.getTasksByUserId);
+router.get("/usertasks", taskController.getTasksByUserId);
+
+/* router.get('/tasks/:id', taskController.getTaskById);
+router.post('/tasks', taskController.createTask);
+router.put('/tasks/:id', taskController.updateTask);
+router.delete('/tasks/:id', taskController.deleteTask);
 
 router.post("/create",
     handleInputError,
