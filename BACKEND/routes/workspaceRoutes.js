1c2228d2498eea409152874d3a119b8476d3f30c
Marouane <marouanelemghari@gmail.com>
2025-04-08 16:40:17 +0100

    Fixed extra declarations

--- BACKEND/routes/workspaceRoutes.js
+++ BACKEND/routes/workspaceRoutes.js
@@ -1,9 +1,6 @@
 import express from "express";
 import { getWorkspacesByuserId } from "../controllers/workspacehandler.js";
-import express from "express";
-import { getWorkspacesByuserId } from "../controllers/workspacehandler.js";
 import { handleInputError } from "../middleware/middleware.js";
-import { getMembersByWorkspaceId } from "../controllers/workspacehandler.js";
 import { getMembersByWorkspaceId } from "../controllers/workspacehandler.js";
 export const routerr = express.Router();
 
