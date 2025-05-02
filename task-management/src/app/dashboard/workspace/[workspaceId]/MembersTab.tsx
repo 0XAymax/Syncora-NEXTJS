979360596c90df7d3d57b15cd530b793d13a75ef
Marouane <marouanelemghari@gmail.com>
2025-05-02 11:21:48 +0100

    Added permissions Modal and filtering by name

--- task-management/src/app/dashboard/workspace/[workspaceId]/MembersTab.tsx
+++ task-management/src/app/dashboard/workspace/[workspaceId]/MembersTab.tsx
@@ -2,16 +2,56 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useState } from "react";
 import { TabsContent } from "@/components/ui/tabs";
 import { useMembersByWorkspace } from "@/hooks/use-workspace";
 import { useAuth } from "@/hooks/use-auth";
 import { InviteDialog } from "./_MembersCRUDComponents/InviteMemberForm";
+import { Workspace } from "@/types";
+import { EditPermissionsForm } from "./_MembersCRUDComponents/EditPermissionsForm";
+import { WorkspaceMember } from "@/lib/types";
+import { User } from "@/hooks/use-auth";
 
-export default function MembersTab({ workspaceId }: { workspaceId: string }) {
+export default function MembersTab({ workspace }: { workspace: Workspace }) {
   const { currentUser } = useAuth();
-  const { data: members } = useMembersByWorkspace(workspaceId);
+  const { data: defMembers } = useMembersByWorkspace(workspace.id);
+  const [members, setMembers] = useState<WorkspaceMember[] | undefined>(
+    defMembers
+  );
+  const [filter, setFilter] = useState<string>("");
+
+  const canChangePermission = (
+    currentUser: User | null,
+    member: WorkspaceMember
+  ) => {
+    if (!currentUser) return false;
+    if (currentUser.id === workspace.ownerId) return true;
+    if (currentUser.role === "admin" && member.id !== workspace.ownerId)
+      return true;
+    return false;
+  };
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value);
+  };
+
+  const filteredMembers =
+    filter && members
+      ? members.filter((m) => {
+          const name = m.user.name.toLowerCase();
+          const email = m.user.email.toLowerCase();
+          const lastName = m.user.lastName.toLowerCase();
+          const loFilter = filter.toLowerCase();
+          return (
+            name.includes(loFilter) ||
+            email.includes(loFilter) ||
+            lastName.includes(loFilter)
+          );
+        })
+      : members;
+  console.log("Members: ", members);
+  console.log("filter: " + filter + "isFilter: " + !!filter);
+  console.log("Filtered Members: ", filteredMembers);
 
   return (
     <TabsContent
@@ -27,17 +67,19 @@ export default function MembersTab({ workspaceId }: { workspaceId: string }) {
             </div>
             <div className="flex items-center gap-2">
               <span className="text-sm text-muted-foreground">Name:</span>
-              <Input className="h-8 w-24" placeholder="Text" />
-              <span className="text-sm text-muted-foreground">Role:</span>
-              <Input className="h-8 w-24" placeholder="Text" />
+              <Input
+                className="h-8 w-24"
+                placeholder="Text"
+                onChange={handleFilterChange}
+              />
             </div>
           </div>
 
-          <InviteDialog workspaceId={workspaceId} members={members} />
+          <InviteDialog workspaceId={workspace.id} members={members} />
 
           <div className="space-y-2 pt-2">
-            {members &&
-              members.map((member, i) => (
+            {filteredMembers &&
+              filteredMembers.map((member, i) => (
                 <div
                   key={i}
                   className="flex items-center justify-between rounded-md border p-2"
@@ -57,18 +99,22 @@ export default function MembersTab({ workspaceId }: { workspaceId: string }) {
                         {member.user.id === currentUser?.id && " (You)"}
                       </div>
                       <div className="text-xs text-muted-foreground">
-                        {member.role}
+                        {member.id === workspace.ownerId
+                          ? "Owner"
+                          : member.role}
                       </div>
                     </div>
                   </div>
-                  <div className="flex gap-2">
-                    <Button variant="outline" size="sm" className="h-7 text-xs">
-                      Attributes...
-                    </Button>
-                    <Button variant="outline" size="sm" className="h-7 text-xs">
-                      Permissions...
-                    </Button>
-                  </div>
+                  {canChangePermission(currentUser, member) && (
+                    <div className="flex gap-2">
+                      <EditPermissionsForm
+                        members={members}
+                        member={member}
+                        setMembers={setMembers}
+                        workspace={workspace}
+                      />
+                    </div>
+                  )}
                 </div>
               ))}
           </div>
